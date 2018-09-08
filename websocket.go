e988793d3561e411d525402616eb8d8328c3ead7
Knut Ahlers <knut@ahlers.me>
2018-09-08 19:00:48 +0200

    Resolve linter issues, improve code quality
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- websocket.go
+++ websocket.go
@@ -8,11 +8,11 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-//Connects to the WebSocket endpoint and waits for incoming notifications
-//This function is designed to run in a goroutine
-//Note: This function clears all notifications after receiving them, so you should pull messages
-//from the Messages channel and save them if you want to keep them
-func (p *POClient) ListenForNotifications() error {
+// ListenForNotifications connects to the WebSocket endpoint and waits for incoming notifications
+// This function is designed to run in a goroutine
+// Note: This function clears all notifications after receiving them, so you should pull messages
+// from the Messages channel and save them if you want to keep them
+func (p Client) ListenForNotifications() error {
 	if !p.loggedIn {
 		return errors.New("Not logged in")
 	}
@@ -23,50 +23,63 @@ func (p *POClient) ListenForNotifications() error {
 	u := url.URL{Scheme: "wss", Host: "client.pushover.net", Path: "/push"}
 
 	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-
 	if err != nil {
 		return err
 	}
 	defer conn.Close()
 
-	conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("login:%s:%s\n", p.device.Id, p.user.Secret)))
+	if err := conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("login:%s:%s\n", p.device.ID, p.user.Secret))); err != nil {
+		return err
+	}
 
 	reconnect := false
 
-	for {
+	for !reconnect {
 		msgType, msgBytes, err := conn.ReadMessage()
+		if err != nil {
+			return err
+		}
 
 		if msgType != websocket.BinaryMessage { //ignore everything else
 			continue
 		}
 
-		if err != nil {
+		if reconnect, err = p.handleNotification(string(msgBytes)); err != nil {
 			return err
-		}
-
-		message := string(msgBytes)
-
-		switch message {
-		case "#": //do nothing
-		case "!":
-			err, messages := p.GetMessages()
-			if err != nil {
-				return err
-			}
-			for _, msg := range messages {
-				p.Messages <- msg //send messages into message channel
-			}
-			p.DeleteOldMessages(&messages)
-		case "R":
-			reconnect = true
-		case "E":
-			return errors.New("Received error frame")
-		}
-
-		if reconnect {
-			break
 		}
 	}
 
 	return p.ListenForNotifications() //reconnect
+}
+
+func (p Client) handleNotification(message string) (reconnect bool, err error) {
+	switch message {
+	// Keep-alive packet, no response needed.
+	case "#":
+		return false, nil
+
+	// A new message has arrived; you should perform a sync.
+	case "!":
+		messages, err := p.GetMessages()
+		if err != nil {
+			return false, err
+		}
+
+		for _, msg := range messages {
+			p.Messages <- msg //send messages into message channel
+		}
+
+		return false, p.DeleteOldMessages(messages)
+
+	// Reload request; you should drop your connection and re-connect.
+	case "R":
+		return true, nil
+
+	// Error; a permanent problem occurred and you should not automatically re-connect. Prompt the user to login again or re-enable the device.
+	case "E":
+		return false, errors.New("Received error frame")
+
+	default:
+		return false, errors.New("Unexpected message received from API")
+	}
 }
